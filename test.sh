699b804d73075a8d90f04fab8c8b83f1d11be070
RAZAFINDRAVONONA Angeli Iriantsoa <iriantsoaangeli@gmail.com>
2026-06-12 11:09:51 +0300

    Changer nom du projet et corriger script

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
-cd "/home/angeli/Asa/Sprint(Mr Naina)/mon-framework"
+cd "/home/angeli/Asa/Sprint(Mr Naina)/spring-framework-by-angeli"
 mvn clean package
-cp ./target/mon-framework-1.0-SNAPSHOT.jar "/home/angeli/Asa/Sprint(Mr Naina)/test/src/main/webapp/WEB-INF/lib"
+cp ./target/spring-framework-by-angeli-1.0-SNAPSHOT.jar "/home/angeli/Asa/Sprint(Mr Naina)/test/src/main/webapp/WEB-INF/lib"
 cd "/home/angeli/Asa/Sprint(Mr Naina)/test"
 mvn clean package
 cp ./target/test-test.war "/home/angeli/Asa/Sprint(Mr Naina)/Tomcat/webapps"
