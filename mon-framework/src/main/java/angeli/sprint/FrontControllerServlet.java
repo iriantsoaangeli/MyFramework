ef4482bc26b7a3f46cadb60ee05b58f0559a5b12
RAZAFINDRAVONONA Angeli Iriantsoa <iriantsoaangeli@gmail.com>
2026-06-12 10:55:12 +0300

    Affichage des exceptions pour le writer de la page

--- mon-framework/src/main/java/angeli/sprint/FrontControllerServlet.java
+++ mon-framework/src/main/java/angeli/sprint/FrontControllerServlet.java
@@ -9,22 +9,19 @@ import jakarta.servlet.http.HttpServletResponse;
 
 public class FrontControllerServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse rep) {
+    protected void doGet(HttpServletRequest req, HttpServletResponse rep) throws IOException {
         ProcessRequest(req, rep);
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse rep) {
+    protected void doPost(HttpServletRequest req, HttpServletResponse rep) throws IOException {
         ProcessRequest(req, rep);
     }
 
-    private void ProcessRequest(HttpServletRequest req, HttpServletResponse rep) {
-        try {
+    private void ProcessRequest(HttpServletRequest req, HttpServletResponse rep) throws IOException {
             PrintWriter wr = rep.getWriter();
             String url = req.getRequestURL().toString();
             wr.print(url);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        
     }
 }
