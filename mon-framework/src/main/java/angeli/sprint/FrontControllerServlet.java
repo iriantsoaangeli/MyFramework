83f42bc2269777c5e52c60cf22dffa669e980c1d
RAZAFINDRAVONONA Angeli Iriantsoa <iriantsoaangeli@gmail.com>
2026-06-12 11:03:53 +0300

    Commentaire d'aide sur la classe et la fonction

--- mon-framework/src/main/java/angeli/sprint/FrontControllerServlet.java
+++ mon-framework/src/main/java/angeli/sprint/FrontControllerServlet.java
@@ -7,6 +7,11 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+/**
+ * Servlet du spring-framework-by-Angeli
+ * @author Angeli
+ * @date 2024/6/11 17:28
+ */
 public class FrontControllerServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse rep) throws IOException {
@@ -18,10 +23,16 @@ public class FrontControllerServlet extends HttpServlet {
         ProcessRequest(req, rep);
     }
 
+
+    /**
+     * Print le URL sur la page web
+     * @param req la requete http
+     * @param rep la reponse http
+      * @throws IOException
+     */
     private void ProcessRequest(HttpServletRequest req, HttpServletResponse rep) throws IOException {
             PrintWriter wr = rep.getWriter();
             String url = req.getRequestURL().toString();
             wr.print(url);
-        
     }
 }
