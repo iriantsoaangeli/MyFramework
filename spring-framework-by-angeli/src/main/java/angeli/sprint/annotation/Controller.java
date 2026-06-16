18ab6e23df989878192326696ddd56d850ba6063
RAZAFINDRAVONONA Angeli Iriantsoa <iriantsoaangeli@gmail.com>
2026-06-16 11:34:36 +0300

    Date fausse dans les commentaires

---
+++ spring-framework-by-angeli/src/main/java/angeli/sprint/annotation/Controller.java
@@ -0,0 +1,11 @@
+package angeli.sprint.annotation;
+
+/**
+ * Annotation pour les controllers
+ * @author Angeli
+ * @date 2026/6/16 11:31 
+ */
+
+public @interface Controller {
+    
+}
--- spring-framework-by-angeli/src/main/java/angeli/sprint/servlet/FrontControllerServlet.java
+++ spring-framework-by-angeli/src/main/java/angeli/sprint/servlet/FrontControllerServlet.java
@@ -2,17 +2,23 @@ package angeli.sprint.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 /**
  * Servlet du spring-framework-by-Angeli
+ * 
  * @author Angeli
- * @date 2024/6/11 17:28
+ * @date 2026/6/11 17:28
  */
 public class FrontControllerServlet extends HttpServlet {
+
+    List<String> controllerList;
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse rep) throws IOException {
         ProcessRequest(req, rep);
@@ -25,15 +31,27 @@ public class FrontControllerServlet extends HttpServlet {
 
     /**
      * Print le URL sur la page web
+     * 
      * @param req la requete http
      * @param rep la reponse http
-      * @throws IOException
-      * @date 2024/6/11 17:29
-      * @author Angeli
+     * @throws IOException
+     * @date 2026/6/11 17:29
+     * @author Angeli
      */
     private void ProcessRequest(HttpServletRequest req, HttpServletResponse rep) throws IOException {
-            PrintWriter wr = rep.getWriter();
-            String url = req.getRequestURL().toString();
-            wr.print(url);
+        PrintWriter wr = rep.getWriter();
+        String url = req.getRequestURL().toString();
+        wr.print(url);
+    }
+
+    /**
+     * Lire les paquets et lister les controller
+     * 
+     * @author Angeli
+     * @date 2026/6/11 17:29
+     */
+    @Override
+    public void init() throws ServletException {
+        super.init();
     }
 }
