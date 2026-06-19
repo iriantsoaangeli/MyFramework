4c48a42a46618eacfb908f641912e116765dda3b
RAZAFINDRAVONONA Angeli Iriantsoa <iriantsoaangeli@gmail.com>
2026-06-19 10:07:11 +0300

    classe pour tester le scan

--- spring-framework-by-angeli/src/main/java/angeli/sprint/App.java
+++ spring-framework-by-angeli/src/main/java/angeli/sprint/App.java
@@ -1,11 +1,13 @@
 package angeli.sprint;
 
+import angeli.sprint.annotation.Controller;
 
 /**
  * Rien pour l'instant
  * @author Angeli
  * @date 2024/6/11 17:28
  */
+@Controller
 public class App 
 {
     public static void main( String[] args )
