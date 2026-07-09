33c8e8222a25f2a0fcf32239aad87ca2f799452d
RAZAFINDRAVONONA Angeli Iriantsoa <iriantsoaangeli@gmail.com>
2026-07-10 00:20:30 +0300

    raccourcir le code avec une classe utilitaire

--- spring-framework-by-angeli/src/main/java/angeli/sprint/servlet/FrontControllerServlet.java
+++ spring-framework-by-angeli/src/main/java/angeli/sprint/servlet/FrontControllerServlet.java
@@ -48,43 +48,10 @@ public class FrontControllerServlet extends HttpServlet {
      * @date 2026/6/11 17:29
      */
     private void ProcessRequest(HttpServletRequest req, HttpServletResponse rep) throws IOException {
-        rep.setContentType("text/html;charset=UTF-8");
-        PrintWriter wr = rep.getWriter();
-        String method = req.getMethod();
-        String[] url = URLParser.getUrlFromRequest(req);
-        String uri = url[1];
-        URLMethod methodPresent = urlMethodMapGET.get(uri);
-        urlMethodMapGET.remove(uri);
-        wr.println("<h2>" + url[0] + " " + url[1] + "</h2>");
-        wr.println("<h3>Les Controllers trouves sont :</h3>");
-        wr.println(controllerList);
-        wr.println("<h3>Les Methodes annotees avec @URL sont :</h3>");
-        wr.println(methodList);
-        wr.println("<h3>Le Map URL -> Method  :</h3>");
-        wr.println(urlMethodMapGET);
-        wr.println("<h3>Votre url : " + url[0] + " " + url[1] + "</h3>");
-        if (methodPresent == null) {
-            wr.println("<h3>Aucune methode n'est associee a cette URL</h3>");
-            return;
-        } else {
-            urlMethodMapGET.put(url[1], methodPresent);
-            if (!methodPresent.getRequestMethod().equals(method)) {
-                wr.println("<h3>Methode appellee :" + methodPresent.getMethod().getName() + "()</h3>");
-            } else {
-
-                methodPresent.getMethod().setAccessible(true);
-                try {
-                    methodPresent.getMethod()
-                            .invoke(methodPresent.getMethod().getDeclaringClass().getDeclaredConstructor()
-                                    .newInstance());
-                    wr.println("<h3>La methode a ete appelee avec succes</h3>");
-                } catch (Exception e) {
-                    e.printStackTrace(wr);
-                }
-            }
-        }
-
+        PageWriter.viewPageNotFound(req, rep, urlMethodMapGET, controllerList, methodList);
     }
+
+
     public void view(HttpServletRequest req, HttpServletResponse rep, String viewName) throws ServletException, IOException {
         String prefix = (String) getServletContext().getAttribute("prefix");
         String affix = (String) getServletContext().getAttribute("affix");
