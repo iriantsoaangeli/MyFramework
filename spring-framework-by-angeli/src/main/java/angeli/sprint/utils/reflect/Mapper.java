84a2a3cf1dc1f93ad43b2aabf36b78f31a658c59
RAZAFINDRAVONONA Angeli Iriantsoa <iriantsoaangeli@gmail.com>
2026-07-09 21:45:05 +0300

    Arret de l'app au scan en cas de doublons

--- spring-framework-by-angeli/src/main/java/angeli/sprint/utils/reflect/Mapper.java
+++ spring-framework-by-angeli/src/main/java/angeli/sprint/utils/reflect/Mapper.java
@@ -16,16 +16,21 @@ public class Mapper {
 
     /**
      * Map les methodes avec l'annotation @URL avec leur URL
+     * 
      * @param methods liste de methodes donnees
      * @return map avec l'URL comme cle et la methode comme valeur
      */
     public Map<String, URLMethod> mapUrlToMethod(List<Method> methods) {
         Map<String, URLMethod> urlMap = new HashMap<>();
-        for (Method method : methods){
-            if(method.isAnnotationPresent(angeli.sprint.annotation.URL.class)){
+        for (Method method : methods) {
+            if (method.isAnnotationPresent(angeli.sprint.annotation.URL.class)) {
                 angeli.sprint.annotation.URL annotation = method.getAnnotation(angeli.sprint.annotation.URL.class);
                 String url = annotation.value();
-                urlMap.put(url, new URLMethod(method, annotation.method()));
+                if (urlMap.containsKey(url) && urlMap.get(url).getRequestMethod().equals(annotation.method())) {
+                    throw new RuntimeException("Doublons sur URL ET METHODE pour la methode " + method.getName() + " et la methode " + urlMap.get(url).getMethod().getName());
+                } else {
+                    urlMap.put(url, new URLMethod(method, annotation.method()));
+                }
             }
         }
         return urlMap;
