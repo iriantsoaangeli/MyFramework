50df35427f75641629e5ec6f9730aef05952053d
RAZAFINDRAVONONA Angeli Iriantsoa <iriantsoaangeli@gmail.com>
2026-07-10 10:43:01 +0300

    donner mapp pour POST et GET

--- spring-framework-by-angeli/src/main/java/angeli/sprint/servlet/PageWriter.java
+++ spring-framework-by-angeli/src/main/java/angeli/sprint/servlet/PageWriter.java
@@ -13,7 +13,8 @@ import jakarta.servlet.http.HttpServletResponse;
 
 public class PageWriter {
     public static void viewPageNotFound(HttpServletRequest req, HttpServletResponse rep,
-            Map<String, URLMethod> urlMethodMapGET, List<String> controllerList, List<Method> methodList)
+            Map<String, URLMethod> urlMethodMapGET, Map<String, URLMethod> urlMethodMapPOST,
+            List<String> controllerList, List<Method> methodList)
             throws IOException {
         rep.setContentType("text/html;charset=UTF-8");
         PrintWriter wr = rep.getWriter();
@@ -21,20 +22,20 @@ public class PageWriter {
         String[] url = URLParser.getUrlFromRequest(req);
         String uri = url[1];
         URLMethod methodPresent = urlMethodMapGET.get(uri);
-        urlMethodMapGET.remove(uri);
         wr.println("<h2>" + url[0] + " " + url[1] + "</h2>");
         wr.println("<h3>Les Controllers trouves sont :</h3>");
         wr.println(controllerList);
         wr.println("<h3>Les Methodes annotees avec @URL sont :</h3>");
         wr.println(methodList);
-        wr.println("<h3>Le Map URL -> Method  :</h3>");
+        wr.println("<h3>Le Map URL et Method  en GET:</h3>");
         wr.println(urlMethodMapGET);
+        wr.println("<h3>Le Map URL et Method  en POST:</h3>");
+        wr.println(urlMethodMapPOST);
         wr.println("<h3>Votre url : " + url[0] + " " + url[1] + "</h3>");
         if (methodPresent == null) {
             wr.println("<h3>Aucune methode n'est associee a cette URL</h3>");
             return;
         } else {
-            urlMethodMapGET.put(url[1], methodPresent);
             if (!methodPresent.getRequestMethod().equals(method)) {
                 wr.println("<h3>Methode appellee :" + methodPresent.getMethod().getName() + "()</h3>");
             } else {
@@ -52,5 +53,5 @@ public class PageWriter {
         }
 
     }
-    
+
 }
