5584bfab34892dfdbb08e5093d9167b33c1ff36c
RAZAFINDRAVONONA Angeli Iriantsoa <iriantsoaangeli@gmail.com>
2026-07-10 10:42:36 +0300

    Probleme ou la liste saute des entrees

--- spring-framework-by-angeli/src/main/java/angeli/sprint/utils/reflect/Mapper.java
+++ spring-framework-by-angeli/src/main/java/angeli/sprint/utils/reflect/Mapper.java
@@ -18,17 +18,18 @@ public class Mapper {
      * Map les methodes avec l'annotation @URL avec leur URL
      * 
      * @param methods liste de methodes donnees
-     * @param Method type de methode (GET,POST)
+     * @param Method  type de methode (GET,POST)
      * @return map avec l'URL comme cle et la methode comme valeur
      */
-    public Map<String, URLMethod> mapUrlToMethod(List<Method> methods,String Method) {
+    public Map<String, URLMethod> mapUrlToMethod(List<Method> methods, String Method) {
         Map<String, URLMethod> urlMap = new HashMap<>();
         for (Method method : methods) {
-            if (method.isAnnotationPresent(angeli.sprint.annotation.URL.class)) {
-                angeli.sprint.annotation.URL annotation = method.getAnnotation(angeli.sprint.annotation.URL.class);
+            angeli.sprint.annotation.URL annotation = method.getAnnotation(angeli.sprint.annotation.URL.class);
+            if (method.isAnnotationPresent(angeli.sprint.annotation.URL.class) && annotation.method().equals(Method)) {
                 String url = annotation.value();
                 if (urlMap.containsKey(url) && urlMap.get(url).getRequestMethod().equals(annotation.method())) {
-                    throw new RuntimeException("Doublons sur URL ET METHODE pour la methode " + method.getName() + " et la methode " + urlMap.get(url).getMethod().getName());
+                    throw new RuntimeException("Doublons sur URL ET METHODE pour la methode " + method.getName()
+                            + " et la methode " + urlMap.get(url).getMethod().getName());
                 } else {
                     urlMap.put(url, new URLMethod(method, annotation.method()));
                 }
@@ -37,24 +38,23 @@ public class Mapper {
         return urlMap;
     }
 
-
     /**
      * Appelle la methode mapUrlToMethod pour les requetes GET
      * 
      * @param methods liste de methodes donnees
      * @return map avec l'URL comme cle et la methode comme valeur
      */
-    public Map<String,URLMethod> mapUrlToMethodGET(List<Method> methods) {
-        return mapUrlToMethod(methods,"GET");
+    public Map<String, URLMethod> mapUrlToMethodGET(List<Method> methods) {
+        return mapUrlToMethod(methods, "GET");
     }
 
-
     /**
-     *Appelle la methode mapUrlToMethod pour les requetes POST
+     * Appelle la methode mapUrlToMethod pour les requetes POST
+     * 
      * @param methods liste de methodes donnees
      * @return map avec l'URL comme cle et la methode comme valeur
      */
-    public Map<String,URLMethod> mapUrlToMethodPOST(List<Method> methods) {
-        return mapUrlToMethod(methods,"POST");
+    public Map<String, URLMethod> mapUrlToMethodPOST(List<Method> methods) {
+        return mapUrlToMethod(methods, "POST");
     }
 }
